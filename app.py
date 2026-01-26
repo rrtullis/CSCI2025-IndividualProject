a88f65af6159b2d099377f2e62212460b7c7795d
rrtullis <reagan.tullis@gmail.com>
2026-01-25 17:48:46 -0700

    sketching out ui

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from shiny.express import input, render, ui
 
-ui.input_slider("val", "Slider label", min=0, max=100, value=50)
+ui.page_opts(title="Mastodon Posts Analysis")
+with ui.nav_panel("Trends"):  
+    "Analysis of trends"
+    ui.input_text("trends_query", "Search query")
+    @render.text
+    def text_trends_query():
+        return input.trends_query()
 
-@render.text
-def slider_val():
-    return f"Slider value: {input.val()}"
+with ui.nav_panel("Sentiment"):  
+    "Analysis of sentiment"
+    ui.input_text("sentiment_query", "Search query")
+    @render.text
+    def text_sentiment_query():
+        return input.sentiment_query()
+
